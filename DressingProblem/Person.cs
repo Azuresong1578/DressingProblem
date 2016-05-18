021a192cf5b6c074d6caddaf4b9e73a203b13872
Xiaofeng Liu <crazywheat808@gmail.com>
2016-05-18 17:27:04 -0400

    working version

---
+++ DressingProblem/Person.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DressingProblem
+{
+    enum TempType { HOT, COLD };
+
+    class Person
+    {
+        private TempType Temperature;
+
+        private bool[] ActionStates;
+
+        private string Output;
+
+        private string[] Commands;
+
+        // constructor: set up the state array that represent each kind of action 
+        public Person()
+        {
+            
+        }
+
+        // execute the input line according to the rules. 
+        // parse the input command into a list and then iterate through
+        // executing each command by calling TryAction.
+        // if any command failed, stop executing by return. 
+        public void ExecuteCommand(string command)
+        {
+            Commands = command.Split(' ', ',');
+            if (Commands.Length < 2)
+            {
+                Console.WriteLine("fail");
+                return;
+            }
+            string temp = Commands[0];
+            if (temp == "HOT")
+            {
+                Temperature = TempType.HOT;
+                ActionStates = new bool[8] { false, false, true, false, true, false, false, true };
+            }
+            else if (temp == "COLD")
+            {
+                Temperature = TempType.COLD;
+                ActionStates = new bool[8] { false, false, false, false, false, false, false, true };
+            }
+            else
+            {
+                Console.WriteLine("fail");
+                return;
+            }
+            for (int i = 1; i < Commands.Length; ++i)
+            {
+                if (Commands[i] != "")
+                {
+                    if (TryAction(Commands[i]) == false)
+                    {
+                        Console.WriteLine(Output);
+                        return;
+                    }
+                }
+            }
+        }
+
+        // private helper function to access the state array.
+        // check the whether a state is true or false.
+        // e.g. state 1 true = footwear is already put on
+        private bool CheckState(int command)
+        {
+            return ActionStates[command - 1];
+        }
+
+        // private helper function to modify the state array.
+        // for each state this function should only be called once.
+        private void ChangeState(int command)
+        {
+            if (command == 8) // Removing PJs
+            {
+                // set it to be false to represent PJs are taken off
+                ActionStates[command - 1] = false;
+            }
+            else // all other actions
+            {
+                ActionStates[command - 1] = true;
+            }
+        }
+
+        // A switch statement to call each command's corresponding
+        // function, will return false if the command is not within
+        // the scope of 1-8
+        private bool TryAction(string command)
+        {
+            switch (command)
+            {
+                case "1":
+                    return TryFootwear();
+                case "2":
+                    return TryHeadwear();
+                case "3":
+                    return TrySocks();
+                case "4":
+                    return TryShirt();
+                case "5":
+                    return TryJacket();
+                case "6":
+                    return TryPants();
+                case "7":
+                    return TryLeaveHouse();
+                case "8":
+                    return TryRmPjs();
+                default:
+                    Output += "fail";
+                    return false;
+            }
+        }
+
+        // Try put on footwear.
+        // Checks PJs off, footwear on, pants and socks on
+        private bool TryFootwear()
+        {
+            if (CheckState(8) == true || CheckState(1) == true 
+                || CheckState(3) == false || CheckState(6) == false)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += (Temperature == TempType.HOT) ? "sandals, " : "boots, ";
+            ChangeState(1);
+            return true;
+        }
+
+        // Try put on headwear.
+        // checks PJs off, headwear not on, shirts on
+        private bool TryHeadwear()
+        {
+            if (CheckState(8) == true || CheckState(2) == true
+                || CheckState(4) == false)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += (Temperature == TempType.HOT) ? "sun visor, " : "hat, ";
+            ChangeState(2);
+            return true;
+        }
+
+        // Try put on socks.
+        // checks PJs off, socks not on, and temperature is cold
+        private bool TrySocks()
+        {
+            if (CheckState(8) == true || CheckState(3) == true
+                || Temperature == TempType.HOT)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += "socks, ";
+            ChangeState(3);
+            return true;
+        }
+
+        // Try put on shirt.
+        // check PJs off, shirt not already on
+        private bool TryShirt()
+        {
+            if (CheckState(8) == true || CheckState(4) == true)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += (Temperature == TempType.HOT) ? "t-shirt, " : "shirt, ";
+            ChangeState(4);
+            return true;
+        }
+
+        // Try put on jacket.
+        // checks PJs off, jacket no already on, temperature is cold,
+        // and shirt is on.
+        private bool TryJacket()
+        {
+            if (CheckState(8) == true || CheckState(5) == true
+                || Temperature == TempType.HOT || CheckState(4) == false)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += "jacket, ";
+            ChangeState(5);
+            return true;
+        }
+
+        // Try put on pants.
+        // checks PJs off, pants not already on
+        private bool TryPants()
+        {
+            if (CheckState(8) == true || CheckState(6) == true)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += (Temperature == TempType.HOT) ? "shorts, " : "pants, ";
+            ChangeState(6);
+            return true;
+        }
+
+        // Try leave the house.
+        // depending on the temperature checks whether the required 
+        // clothes are put on. This function returns false not matter what,
+        // signal the caller the end of a series of command. 
+        private bool TryLeaveHouse()
+        {
+            if (Temperature == TempType.HOT)
+            {
+                if (CheckState(1) && CheckState(2) && CheckState(4) && CheckState(6))
+                {
+                    Output += "leaving house";
+                }
+                else
+                {
+                    Output += "fail";
+                }
+            }
+            else
+            {
+                if (CheckState(1) && CheckState(2) && CheckState(3)
+                    && CheckState(4) && CheckState(5) && CheckState(6))
+                {
+                    Output += "leaving house";
+                }
+                else
+                {
+                    Output += "fail";
+                }
+            }
+            return false;
+        }
+
+        // Try to remove PJs,
+        // false if PJs already removed. 
+        private bool TryRmPjs()
+        {
+            if (CheckState(8) == false)
+            {
+                Output += "fail";
+                return false;
+            }
+            Output += "Removing PJs, ";
+            ChangeState(8);
+            return true;
+        }
+    }
+}
---
+++ DressingProblem/Test.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DressingProblem
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // successful
+            Person p1 = new Person();
+            p1.ExecuteCommand("HOT 8, 4, 6, 1, 2, 7");
+            // successful
+            Person p2 = new Person();
+            p2.ExecuteCommand("HOT 8, 6, 4, 2, 1, 7");
+            // successful
+            Person p3 = new Person();
+            p3.ExecuteCommand("COLD 8, 4, 5, 2, 6, 3, 1, 7");
+            // successful
+            Person p4 = new Person();
+            p4.ExecuteCommand("COLD 8, 6, 3, 1, 4, 5, 2, 7");
+            // invalid temperature command
+            Person p5 = new Person();
+            p5.ExecuteCommand("WARM 8, 4, 6, 1, 2, 7");
+            // invalid command that doesn't end properly
+            Person p6 = new Person();
+            p6.ExecuteCommand("HOT");
+            // try leave house directly
+            Person p7 = new Person();
+            p7.ExecuteCommand("HOT 7");
+            // try to wear sock in hot temperature
+            Person p8 = new Person();
+            p8.ExecuteCommand("HOT 8, 6, 3, 1, 4, 1, 2, 7");
+            // try to wear jacket in hot temperature
+            Person p9 = new Person();
+            p9.ExecuteCommand("HOT 8, 4, 5, 6, 1, 2, 7");
+            // try to not wear socks and jacket in cold
+            Person p10 = new Person();
+            p10.ExecuteCommand("COLD 8, 4, 6, 1, 2, 7");
+            // take off PJs twice
+            Person p11 = new Person();
+            p11.ExecuteCommand("HOT 8, 4, 6, 8, 1, 2, 7");
+            // wear the same cloth twice
+            Person p12 = new Person();
+            p12.ExecuteCommand("COLD 8, 4, 6, 5, 5, 1, 2, 7");
+            // not taking off PJs before wearing clothes
+            Person p13 = new Person();
+            p13.ExecuteCommand("COLD 4, 6, 5, 5, 1, 2, 7");
+            // wear shoes before socks
+            Person p14 = new Person();
+            p14.ExecuteCommand("COLD 8, 4, 6, 5, 2, 1, 3, 7");
+            // wear shoes before pants
+            Person p15 = new Person();
+            p15.ExecuteCommand("COLD 8, 4, 5, 2, 3, 1, 6, 7");
+            // wear jacket before shirt
+            Person p16 = new Person();
+            p16.ExecuteCommand("COLD 8, 5, 4, 2, 3, 1, 6, 7");
+            // wear hat before shirt
+            Person p17 = new Person();
+            p17.ExecuteCommand("COLD 8, 2, 5, 4, 3, 1, 6, 7");
+        }
+    }
+}
